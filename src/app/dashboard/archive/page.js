fc23848dfe781219e47e1cc304c794f704f10a26
LaksheyHooda <lakshey2016@gmail.com>
2024-04-27 16:38:41 -0700

    added rest of the paths

---
+++ src/app/dashboard/archive/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import { db, auth } from "@/config/firebase/config";
+
+export default function Home() {
+
+    return (
+        <div className="flex items-center justify-center h-screen w-screen">
+            <h1 className="text-center">Welcome to Archive</h1>
+        </div>
+    );
+}

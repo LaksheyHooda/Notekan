84d404d6b072c3b728e3b41749d19d4372c819de
LaksheyHooda <lakshey2016@gmail.com>
2024-04-27 16:59:25 -0700

    more stuff

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,97 +1,132 @@
 "use client";
 
 import { useEffect, useState, useRef } from "react";
+import { db, auth } from "@/config/firebase/config";
 import { Button } from "@nextui-org/react";
 import { useRecordVoice } from "@/hooks/useRecordVoice";
 import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
 import { faSquare } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function Dashboard() {
-  const [isRecording, setIsRecording] = useState(false);
-  const mediaRecorderRef = useRef(null);
-  const webSocketRef = useRef(null);
-  const { startRecording, stopRecording, text } = useRecordVoice();
+    const [isRecording, setIsRecording] = useState(false);
+    const [userID, setUserID] = useState("");
+    const mediaRecorderRef = useRef(null);
+    const webSocketRef = useRef(null);
+    const { startRecording, stopRecording, text } = useRecordVoice();
 
-  useEffect(() => {
-    // Create WebSocket connection.
-    // webSocketRef.current = new WebSocket('ws://localhost:8080');
-    // // Commands for WebSocket events
-    // webSocketRef.current.onopen = () => {
-    //     console.log('WebSocket Client Connected');
-    // };
-    // webSocketRef.current.onclose = () => {
-    //     console.log('WebSocket Client Disconnected');
-    // };
-    // return () => {
-    //     webSocketRef.current.close();
-    // };
-  }, []);
 
-  const startRecordingWebsock = async () => {
-    if (webSocketRef.current.readyState === WebSocket.CLOSED) {
-      console.log("WebSocket is closed");
-      webSocketRef.current = new WebSocket("ws://localhost:8080");
-    }
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/auth.user
+                const uid = user.uid;
+                setUserID(uid);
+                console.log("uid", uid);
+            } else {
+                // User is signed out
+                // ...
+                console.log("user is signed out");
+            }
+        });
+    }, []);
 
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      mediaRecorderRef.current = new MediaRecorder(stream);
-      mediaRecorderRef.current.start();
-
-      // Collect audio data as it comes in
-      mediaRecorderRef.current.ondataavailable = (event) => {
-        if (webSocketRef.current.readyState === WebSocket.OPEN) {
-          webSocketRef.current.send(event.data);
+    const startRecordingWebsock = async () => {
+        if (webSocketRef.current.readyState === WebSocket.CLOSED) {
+            console.log("WebSocket is closed");
+            webSocketRef.current = new WebSocket("ws://localhost:8080");
         }
-      };
 
-      mediaRecorderRef.current.onstop = () => {
-        console.log("MediaRecorder stopped");
-        webSocketRef.current.close();
-      };
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            mediaRecorderRef.current = new MediaRecorder(stream);
+            mediaRecorderRef.current.start();
 
-      setIsRecording(true);
-    } catch (error) {
-      console.error("Error accessing audio devices.", error);
-    }
-  };
+            // Collect audio data as it comes in
+            mediaRecorderRef.current.ondataavailable = (event) => {
+                if (webSocketRef.current.readyState === WebSocket.OPEN) {
+                    webSocketRef.current.send(event.data);
+                }
+            };
 
-  const stopRecordingWebsock = async () => {
-    mediaRecorderRef.current.stop();
-    setIsRecording(false);
-    // Close the media stream
-    mediaRecorderRef.current.stream
-      .getTracks()
-      .forEach((track) => track.stop());
-  };
+            mediaRecorderRef.current.onstop = () => {
+                console.log("MediaRecorder stopped");
+                webSocketRef.current.close();
+            };
 
-  const handleButtonClick = () => {
-    console.log("clicked");
-    if (isRecording) {
-      stopRecording();
-      setIsRecording(false);
-    } else {
-      startRecording();
-      setIsRecording(true);
-    }
-  };
+<<<<<<< HEAD
+            const handleButtonClick = () => {
+                console.log("clicked");
+                if (isRecording) {
+                    stopRecording();
+                    setIsRecording(false);
+                } else {
+                    startRecording();
+                    setIsRecording(true);
+                }
+            };
 
-  return (
-    <div className="flex justify-center inset-0 fixed items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500">
-      <Button
-        onClick={handleButtonClick}
-        className={`w-20 h-20 bg-red-500 hover:bg-red-600 text-white rounded-full flex items-center justify-center transition-all duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 ${
-          isRecording ? "animate-pulse" : ""
-        }`}
-      >
-        {isRecording ? (
-          <FontAwesomeIcon className="h-6 w-6" icon={faSquare} />
-        ) : (
-          <FontAwesomeIcon className="h-6 w-6" icon={faMicrophone} />
-        )}
-      </Button>
-      <p>{text}</p>
-    </div>
-  );
-}
+            return (
+                <div className="flex justify-center inset-0 fixed items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500">
+                    <Button
+                        onClick={handleButtonClick}
+                        className={`w-20 h-20 bg-red-500 hover:bg-red-600 text-white rounded-full flex items-center justify-center transition-all duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 ${isRecording ? "animate-pulse" : ""
+                            }`}
+                    >
+                        {isRecording ? (
+                            <FontAwesomeIcon className="h-6 w-6" icon={faSquare} />
+                        ) : (
+                            <FontAwesomeIcon className="h-6 w-6" icon={faMicrophone} />
+                        )}
+                    </Button>
+                    <p>{text}</p>
+                </div>
+            );
+=======
+            setIsRecording(true);
+        } catch (error) {
+            console.error("Error accessing audio devices.", error);
+        }
+    };
+
+    const stopRecordingWebsock = async () => {
+        mediaRecorderRef.current.stop();
+        setIsRecording(false);
+        // Close the media stream
+        mediaRecorderRef.current.stream
+            .getTracks()
+            .forEach((track) => track.stop());
+    };
+
+    const handleButtonClick = () => {
+        console.log("clicked");
+        if (isRecording) {
+            stopRecording();
+            setIsRecording(false);
+        } else {
+            startRecording();
+            setIsRecording(true);
+        }
+    };
+
+    return (
+        <div className="flex justify-center inset-0 fixed items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500">
+            <Button
+                onClick={handleButtonClick}
+                className={
+                    "w-20 h-20 bg-red-500 hover:bg-red-600 text-white rounded-full flex items-center justify-center transition-all duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 ${isRecording ? 'animate-pulse' : ''}"
+                }
+            >
+                {isRecording ? (
+                    <FontAwesomeIcon className="h-6 w-6" icon={faSquare} />
+                ) : (
+                    <FontAwesomeIcon className="h-6 w-6" icon={faMicrophone} />
+                )}
+            </Button>
+            <p>{text}</p>
+        </div>
+    );
+>>>>>>> 7caa324 (stuff)
+        }
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { db, auth } from "@/config/firebase/config";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, setDoc, collection } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { Input } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
@@ -10,7 +10,7 @@ import Modal from "@/components/failedloginmodal";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 import { useRouter } from "next/navigation";
-import { Link } from "@nextui-org/react"; 
+import { Link } from "@nextui-org/react";
 
 export default function Signup() {
     const [email, setEmail] = useState("");
@@ -24,8 +24,9 @@ export default function Signup() {
     const handleSignup = async () => {
         try {
             await createUserWithEmailAndPassword(auth, email, password).then(
-                (userCredential) => {
-                    router.replace(`/login`);
+                async (userCredential) => {
+                    await createUserInFirestore(userCredential.user);
+                    //router.replace(`/login`);
                 }
             );
         } catch (error) {
@@ -40,6 +41,22 @@ export default function Signup() {
         }
     }
 
+    const createUserInFirestore = async (user) => {
+        const docData = {
+            uid: user.uid,
+            email: user.email,
+            processedDocsIDs: [],
+            username: "empty",
+            rawDocsIDs: [],
+        };
+        try {
+            await addDoc(collection(db, "users"), docData);
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+        //await setDoc(doc(db, "user"), docData);
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
         handleSignup();
@@ -52,46 +69,46 @@ export default function Signup() {
     };
 
     return (
-      <main className="flex justify-center inset-0 fixed items-center h-screen bg-gradient-to-r from-[#006e96] to-[#243c5a]">
-        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-          <h2 className="text-2xl font-bold mb-4 text-slate-900">Sign Up</h2>
-          <form onSubmit={handleSubmit}>
-            <Input
-              className="mt-4 w-full rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-text caret-blue-500 animate-blink-wide"
-              placeholder="Email"
-              onKeyDown={handleKeyDown}
-              value={email}
-              onValueChange={setEmail}
-              required
-            />
-            <Input
-              className="mt-4 w-full rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-text caret-blue-500 animate-blink-wide"
-              placeholder="Password"
-              type="password"
-              onKeyDown={handleKeyDown}
-              value={password}
-              onValueChange={setPassword}
-              required
-            />
-            <Button
-              onClick={handleSubmit}
-              className="py-2 px-4 mt-4 w-full bg-gray-300 hover:bg-gray-400 text-gray-800 shadow-gray-500/50 font-bold"
-            >
-              Sign Up
-            </Button>
-          </form>
-          <div className="mt-4 text-center">
-            <span className="mr-2 text-slate-900">
-              Already have an account?
-            </span>
-            <Link
-              href="/login"
-              className="text-blue-500 hover:text-blue-700 font-medium"
-            >
-              Log in
-            </Link>
-          </div>
-        </div>
-      </main>
+        <main className="flex justify-center inset-0 fixed items-center h-screen bg-gradient-to-r from-[#006e96] to-[#243c5a]">
+            <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+                <h2 className="text-2xl font-bold mb-4 text-slate-900">Sign Up</h2>
+                <form onSubmit={handleSubmit}>
+                    <Input
+                        className="mt-4 w-full rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-text caret-blue-500 animate-blink-wide"
+                        placeholder="Email"
+                        onKeyDown={handleKeyDown}
+                        value={email}
+                        onValueChange={setEmail}
+                        required
+                    />
+                    <Input
+                        className="mt-4 w-full rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-text caret-blue-500 animate-blink-wide"
+                        placeholder="Password"
+                        type="password"
+                        onKeyDown={handleKeyDown}
+                        value={password}
+                        onValueChange={setPassword}
+                        required
+                    />
+                    <Button
+                        onClick={handleSubmit}
+                        className="py-2 px-4 mt-4 w-full bg-gray-300 hover:bg-gray-400 text-gray-800 shadow-gray-500/50 font-bold"
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+                <div className="mt-4 text-center">
+                    <span className="mr-2 text-slate-900">
+                        Already have an account?
+                    </span>
+                    <Link
+                        href="/login"
+                        className="text-blue-500 hover:text-blue-700 font-medium"
+                    >
+                        Log in
+                    </Link>
+                </div>
+            </div>
+        </main>
     );
 }

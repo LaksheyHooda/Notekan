da9be4def3fce9d5b9f34247aaf154ceb8c5722d
LaksheyHooda <lakshey2016@gmail.com>
2024-04-27 16:26:08 -0700

    changed stuff in route

--- src/app/api/speechToText/route.js
+++ src/app/api/speechToText/route.js
@@ -1,13 +1,26 @@
 import { NextResponse } from "next/server";
 import fs from "fs";
-import * as dotenv from "dotenv";
 import OpenAI from "openai";
-
-dotenv.config();
 
 const openai = new OpenAI({
     apiKey: 'sk-proj-0CwNwWN0Po6pi6EU857tT3BlbkFJNHIJrbixJn5QZZbHo0LJ',
 });
+
+async function performGeneralSummaryTemplate(data) {
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: "You are a helpful assistant designed to summerize meetings with a title and with a bulleted summary that returns JSON.",
+            },
+            { role: "user", content: data.text },
+        ],
+        model: "gpt-3.5-turbo-0125",
+        response_format: { type: "json_object" },
+    });
+    console.log(data.text);
+    console.log(completion.choices[0].message.content);
+}
 
 export async function POST(req) {
     const body = await req.json();
@@ -24,7 +37,7 @@ export async function POST(req) {
         });
         // Remove the file after use
         fs.unlinkSync(filePath);
-
+        await performGeneralSummaryTemplate(data)
         return NextResponse.json(data);
     } catch (error) {
         console.error("Error processing audio:", error);

3dc46767432c1ca0a71040f880720d36a7ed215a
Torin Samples <dragonclaw339@gmail.com>
2024-04-26 20:17:54 -0700

    sign in/sign up page finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,101 @@
-import Image from "next/image";
+'use client'
+
+import { db, auth } from "@/config/firebase/config";
+import { addDoc, collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Input } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import Modal from "@/components/failedloginmodal";
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
+import { useRouter } from 'next/navigation'
 import { Link } from "@nextui-org/react";
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter()
+
+  useEffect(() => {
+
+  }, []);
+
+  const handleSignup = async () => {
+      try {
+          await signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+              //revalidatePath('/dashboard') // Update cached posts
+              router.replace(`/dashboard`)
+          })
+      } catch (error) {
+          setError(error.message);
+          setIsModalOpen(true);
+      }
+  }
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      handleSignup();
+  }
+
+  const handleKeyDown = (event) => {
+      if (event.key === "Enter") {
+          handleSubmit(event);
+      }
+  }
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex-col inline-block">
-        <p className="mb-4">Welcome to Notekan</p>
-        <div>
-          <Link href="/signup" className="mb-4">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Sign Up
-            </button>
-          </Link>
-        </div>
-        <div>
-          <Link href="/login" className="mb-4">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Login
-            </button>
+    <main className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        <h2 className="text-2xl font-bold text-slate-900 mb-4">
+          Welcome to Notekan!
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <Input
+            className="mt-4 w-full rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-text caret-blue-500 animate-blink-wide"
+            placeholder="Email"
+            onKeyDown={handleKeyDown}
+            value={email}
+            onValueChange={setEmail}
+            required
+          />
+          <Input
+            className="mt-4 w-full rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-text caret-blue-500 animate-blink-wide"
+            placeholder="Password"
+            type="password"
+            onKeyDown={handleKeyDown}
+            value={password}
+            onValueChange={setPassword}
+            required
+          />
+          <div className="flex justify-between items-center mb-4">
+            <div>
+              <input type="checkbox" id="remember" name="remember" className="mr-2" />
+              <label htmlFor="remember" className="font-medium text-slate-900">
+                Remember me
+              </label>
+            </div>
+            <Link href="/forgotPassword" className="text-blue-500 hover:text-blue-700 font-medium">
+              Forgot password?
+            </Link>
+          </div>
+          <Button className="py-2 px-4 w-full bg-gray-300 hover:bg-gray-400 text-gray-800 shadow-gray-500/50 font-bold">
+            Login
+          </Button>
+        </form>
+
+        <Modal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          message={error}
+        />
+
+        <div className="mt-4 text-center">
+          <span className="mr-2 text-slate-900">Don't have an account?</span>
+          <Link href="/signup" className="text-blue-500 hover:text-blue-700 font-medium">
+            Sign up
           </Link>
         </div>
       </div>

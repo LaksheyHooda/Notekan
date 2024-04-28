a44de10e089d06a8e16e22f83e32a6f04c313782
LaksheyHooda <lakshey2016@gmail.com>
2024-04-28 03:29:05 -0700

    added process page selections

--- src/app/dashboard/processed/page.js
+++ src/app/dashboard/processed/page.js
@@ -45,7 +45,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col pt-10 inset-0 fixed h-screen bg-gradient-to-r from-blue-500 to-purple-500">
+    <div className="flex flex-col pt-10 inset-0 w-screen min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
       <header>
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg-px:6">
           <h1 className="text-3xl font-bold text-white">
@@ -58,65 +58,27 @@ export default function Home() {
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-              <Link
-                href="#"
-                className="bg-white shadow rounded-lg overflow-hidden"
-              >
-                <div className="h-40 bg-cover bg-center"></div>
-                <div className="p-4">
-                  <h3 className="text-lg font-medium text-gray-900">
-                    File Name.pdf
-                  </h3>
-                  <p className="text-sm text-gray-500">
-                    Last Modified: April 28, 2024
-                  </p>
-                </div>
-              </Link>
 
-              <Link
-                href="#"
-                className="bg-white shadow rounded-lg overflow-hidden"
-              >
-                <div className="h-40 bg-cover bg-center"></div>
-                <div className="p-4">
-                  <h3 className="text-lg font-medium text-gray-900">
-                    Image.jgp
-                  </h3>
-                  <p className="text-sm text-gray-500">
-                    Last Modified: April 27, 2024
-                  </p>
-                </div>
-              </Link>
-
-              <Link
-                href="#"
-                className="bg-white shadow rounded-lg overflow-hidden"
-              >
-                <div className="h-40 bg-cover bg-center"></div>
-                <div className="p-4">
-                  <h3 className="text-lg font-medium text-gray-900">
-                    File Name.pdf
-                  </h3>
-                  <p className="text-sm text-gray-500">
-                    Last Modified: April 28, 2024
-                  </p>
-                </div>
-              </Link>
-
-              <Link
-                href="#"
-                className="bg-white shadow rounded-lg overflow-hidden"
-              >
-                <div className="h-40 bg-cover bg-center"></div>
-                <div className="p-4">
-                  <h3 className="text-lg font-medium text-gray-900">
-                    Spreadsheet.xlsx
-                  </h3>
-                  <p className="text-sm text-gray-500">
-                    Last Modified: April 25, 2024
-                  </p>
-                </div>
-              </Link>
+              {rawDocs.map((doc) => (
+                 <Link
+                 key={doc.id}
+                 href="#"
+                 className="bg-white shadow rounded-lg overflow-hidden"
+               >
+                 <div className="h-40 bg-cover bg-center"></div>
+                 <div className="p-4">
+                   <h3 className="text-lg font-medium text-gray-900">
+                     {doc.data().title}
+                   </h3>
+                   <p className="text-sm text-gray-500">
+                     Date Added: {new Date(doc.data().time).toLocaleString()}
+                   </p>
+                   <p className="text-md text-gray-500">
+                     Type: {doc.data().type}
+                   </p>
+                 </div>
+               </Link>
+              ))}
             </div>
           </div>
         </div>

dd24212d2df416b0a7e9b50a9b1b7b1f8e0eb47a
TorinSamples <torin.samples@gmail.com>
2024-04-28 01:20:39 -0700

    error handling

--- src/components/lefthandnavbar.js
+++ src/components/lefthandnavbar.js
@@ -44,23 +44,23 @@ export default function Sidebar() {
             <path
               d="M4 6H20"
               stroke="#000000"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
             <path
               d="M4 12H20"
               stroke="#000000"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
             <path
               d="M4 18H20"
               stroke="#000000"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
         )}
@@ -74,23 +74,23 @@ export default function Sidebar() {
           <div className="absolute top-4 right-4">
             <Button onClick={toggleSidebar} className="bg-gray-200 hover:bg-gray-300 rounded-full p-2 focus:outline-none w-10 h-10">
               <svg
-                class="w-6 h-6 text-gray-600"
+                className="w-6 h-6 text-gray-600"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M6 18L18 6M6 6l12 12"
                 ></path>
               </svg>
             </Button>
           </div>
           <div>
-            <div className="mt-12 md:border-2 rounded-full p-4">
+            <div className="mt-12   md:border-2 rounded-full p-4">
               <Link href="/" className="md:w-48 md:h-48 max-w-full h-auto">
               <img
                 src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1224184972.1714089600&semt=sph"
@@ -113,9 +113,9 @@ export default function Sidebar() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                   ></path>
                 </svg>
@@ -136,102 +136,102 @@ export default function Sidebar() {
                     d="M6 7V21H18V7H6Z"
                     fill="#F3F4F6"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinejoin="round"
                   />
                   <path
                     d="M6 7V3H14L18 7V21H6V7Z"
                     fill="#FFFFFF"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinejoin="round"
                   />
                   <path
                     d="M14 3V7H18"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                   <path
                     d="M10 11H14"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
                   />
                   <path
                     d="M10 15H14"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
                   />
 
                   <path
                     d="M5 6V20H17V6H5Z"
                     fill="#F3F4F6"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinejoin="round"
                   />
                   <path
                     d="M5 6V2H13L17 6V20H5V6Z"
                     fill="#FFFFFF"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinejoin="round"
                   />
                   <path
                     d="M13 2V6H17"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                   <path
                     d="M9 10H13"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
                   />
                   <path
                     d="M9 14H13"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
                   />
 
                   <path
                     d="M4 5V19H16V5H4Z"
                     fill="#F3F4F6"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinejoin="round"
                   />
                   <path
                     d="M4 5V1H12L16 5V19H4V5Z"
                     fill="#FFFFFF"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinejoin="round"
                   />
                   <path
                     d="M12 1V5H16"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                   <path
                     d="M8 9H12"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
                   />
                   <path
                     d="M8 13H12"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
                   />
                 </svg>
                 <span className="font-medium text-gray-800">Documents</span>
@@ -272,9 +272,9 @@ export default function Sidebar() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M13 10V3L4 14h7v7l9-11h-7z"
                   ></path>
                 </svg>
@@ -296,28 +296,28 @@ export default function Sidebar() {
                   <path
                     d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                   <circle
                     cx="9"
                     cy="7"
                     r="4"
                     stroke="#6B7280"
-                    stroke-width="2"
+                    strokeWidth="2"
                   />
                   <path
                     d="M23 21v-2a4 4 0 0 0-3-3.87"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
                   />
                   <path
                     d="M16 3.13a4 4 0 0 1 0 7.75"
                     stroke="#6B7280"
-                    stroke-width="2"
-                    stroke-linecap="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
                   />
                 </svg>
                 <span className="font-medium text-gray-800">About</span>
@@ -337,9 +337,9 @@ export default function Sidebar() {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                 ></path>
               </svg>
@@ -357,15 +357,15 @@ export default function Sidebar() {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
                 ></path>
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                 ></path>
               </svg>

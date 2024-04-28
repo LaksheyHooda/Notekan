8ec94a20a6a6df275349dada732c9e8077852bc3
LaksheyHooda <lakshey2016@gmail.com>
2024-04-28 03:18:40 -0700

    changes

--- src/app/dashboard/processed/page.js
+++ src/app/dashboard/processed/page.js
@@ -2,10 +2,50 @@
 
 import { db, auth } from "@/config/firebase/config";
 import { Link } from "@nextui-org/react";
+import { Button, DropdownTrigger, Input } from "@nextui-org/react";
+import { Dropdown, DropdownMenu, DropdownItem } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { onAuthStateChanged } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  getDoc,
+  doc,
+} from "firebase/firestore";
 
 export default function Home() {
+  const [rawDocs, setRawDocs] = useState([]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        getItems(user.uid);
+      } else {
+        router.replace(`/login`);
+      }
+    });
+  }, []);
+
+  const getItems = async (userID) => {
+    try {
+      const q = query(
+        collection(db, "processedDocuments"),
+        where("userID", "==", userID)
+      );
+      const querySnapshot = await getDocs(q);
+      setRawDocs(querySnapshot.docs);
+      console.log("Query Snapshot: ", querySnapshot.docs);
+    } catch (error) {
+      console.error("Error: ", error);
+      return null;
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center inset-0 fixed h-screen bg-gradient-to-r from-blue-500 to-purple-500">
+    <div className="flex flex-col pt-10 inset-0 fixed h-screen bg-gradient-to-r from-blue-500 to-purple-500">
       <header>
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg-px:6">
           <h1 className="text-3xl font-bold text-white">

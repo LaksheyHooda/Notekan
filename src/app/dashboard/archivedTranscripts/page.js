704b5e2dc1478bb3a178324c7ccd4c56f4f86a96
Torin Samples <dragonclaw339@gmail.com>
2024-05-11 17:29:36 -0700

    delete

--- src/app/dashboard/archivedTranscripts/page.js
+++ src/app/dashboard/archivedTranscripts/page.js
@@ -6,152 +6,206 @@ import { Dropdown, DropdownMenu, DropdownItem } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { onAuthStateChanged } from "firebase/auth";
+import { Link } from "@nextui-org/react";
+import { update } from "firebase/database";
 import {
-  collection,
-  getDocs,
-  query,
-  where,
-  getDoc,
-  doc,
+	collection,
+	getDocs,
+	query,
+	where,
+	doc,
+	deleteDoc,
+	FieldValue,
+	arrayRemove,
 } from "firebase/firestore";
 
 export default function Archive() {
-  const router = useRouter();
-  const [searchTerm, setSearchTerm] = useState("");
-  const [sortBy, setSortBy] = useState("Date");
-  const [items, setItems] = useState([]);
-  const [rawDocs, setRawDocs] = useState([]);
-  const [dropdownEnabled, setDropdownEnables] = useState(true);
+	const router = useRouter();
+	const [searchTerm, setSearchTerm] = useState("");
+	const [sortBy, setSortBy] = useState("Date");
+	const [items, setItems] = useState([]);
+	const [rawDocs, setRawDocs] = useState([]);
+	const [dropdownEnabled, setDropdownEnables] = useState(true);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        getItems(user.uid);
-      } else {
-        router.replace(`/login`);
-      }
-    });
-  }, []);
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				getItems(user.uid);
+			} else {
+				router.replace(`/login`);
+			}
+		});
+	}, []);
 
-  const getItems = async (userID) => {
-    try {
-      const q = query(
-        collection(db, "transcriptions"),
-        where("userID", "==", userID)
-      );
-      const querySnapshot = await getDocs(q);
-      setRawDocs(querySnapshot.docs);
-      setItems(querySnapshot.docs);
-    } catch (error) {
-      console.error("Error: ", error);
-      return null;
-    }
-  };
+	const getItems = async (userID) => {
+		try {
+			const q = query(
+				collection(db, "transcriptions"),
+				where("userID", "==", userID)
+			);
+			const querySnapshot = await getDocs(q);
+			setRawDocs(querySnapshot.docs);
+			setItems(querySnapshot.docs);
+		} catch (error) {
+			console.error("Error: ", error);
+			return null;
+		}
+	};
 
-  const handleSwitchTerm = (e) => {
-    setSortBy(e);
+	const handleDeleteRow = async (id) => {
+		try {
+			const collectionRef = collection(db, "transcriptions");
+			const q = query(
+				collectionRef,
+				where("userID", "==", auth.currentUser.uid),
+			);
 
-    if (e === "Date") {
-      setItems(
-        [...rawDocs].sort(
-          (a, b) => new Date(b.data().time) - new Date(a.data().time)
-        )
-      );
-    } else if (e === "Title") {
-      setItems(
-        [...rawDocs].sort((a, b) =>
-          a.data().title.localeCompare(b.data().title)
-        )
-      );
-    }
-  };
+			const querySnapshot = await getDocs(q);
 
-  useEffect(() => {
-    const filteredItems = rawDocs.filter((doc) =>
-      doc.data().title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setItems(filteredItems);
-  }, [searchTerm, rawDocs]);
+			if (!querySnapshot.empty) {
+				console.log("Deleting: ", id);	
+				const docRef = doc(db, "transcriptions", id);
+				await deleteDoc(docRef);
+				getItems(auth.currentUser.uid);
+			}
+		} catch (error) {
+			console.error("Error: ", error);
+			return null;
+		}
+	};
 
-  return (
-    <div className="flex w-screen min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
-      <div className="container justify-center mx-auto py-8">
-        <h1 className="text-4xl font-bold mb-6 text-center text-white">
-          NoteKan Script Archive
-        </h1>
+	const handleSwitchTerm = (e) => {
+		setSortBy(e);
 
-        <div className="mb-6 flex justify-center space-x-4">
-          <Input
-            type="text"
-            placeholder="Search..."
-            className="rounded-md px-4 py-2 w-full max-w-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-text caret-blue-500 animate-blink-wide"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
+		if (e === "Date") {
+			setItems(
+				[...rawDocs].sort(
+					(a, b) => new Date(b.data().time) - new Date(a.data().time)
+				)
+			);
+		} else if (e === "Title") {
+			setItems(
+				[...rawDocs].sort((a, b) =>
+					a.data().title.localeCompare(b.data().title)
+				)
+			);
+		}
+	};
 
-          <Dropdown isDisabled={!dropdownEnabled}>
-            <DropdownTrigger>
-              <Button className="rounded-md px-4 py-2 text-black">
-                Sort By: {sortBy}
-              </Button>
-            </DropdownTrigger>
-            <DropdownMenu
-              aria-label="Sort Options"
-              variant="flat"
-              selectionMode="single"
-              className="text-black"
-              onAction={(e) => handleSwitchTerm(e)}
-            >
-              <DropdownItem key="Date">Date</DropdownItem>
-              <DropdownItem key="Title">Title</DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </div>
+	useEffect(() => {
+		const filteredItems = rawDocs.filter((doc) =>
+			doc.data().title.toLowerCase().includes(searchTerm.toLowerCase())
+		);
+		setItems(filteredItems);
+	}, [searchTerm, rawDocs]);
 
-        <div className="overflow-x-auto">
-          <table className="w-full max-w-3xl border-collapse mx-auto bg-white rounded-lg shadow-lg">
-            <thead>
-              <tr className="bg-gray-200 text-center">
-                <th
-                  className="py-2 px-4 text-black rounded-tl-lg"
-                  style={{ width: "80%" }}
-                >
-                  Title
-                </th>
-                <th
-                  className="py-2 px-4 text-black rounded-tr-lg"
-                  style={{ width: "20%" }}
-                >
-                  Date
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {items.map((doc) => (
-                <tr
-                  key={doc.id}
-                  className="border-b hover:bg-gray-100 transition-colors duration-300"
-                >
-                  <td className="pb-2 pt-2 pr-4" style={{ width: "80%" }}>
-                    <a
-                      href={"/dashboard/archivedtranscripts/transcriptcontent?id=" + doc.id}
-                      className="text-blue-500 hover:text-blue-700 transition-colors duration-300"
-                    >
-                      {doc.data().title}
-                    </a>
-                  </td>
-                  <td
-                    className="text-black pb-2 pt-2 pl-4"
-                    style={{ width: "20%" }}
-                  >
-                    {new Date(doc.data().time).toLocaleString()}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex w-screen min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
+			<div className="container justify-center mx-auto py-8">
+				<h1 className="text-4xl font-bold mb-6 text-center text-white">
+					NoteKan Script Archive
+				</h1>
+
+				<div className="mb-8 flex justify-center space-x-4">
+					<Input
+						type="text"
+						placeholder="Search..."
+						className="rounded-md px-4 py-2 w-full max-w-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-text caret-blue-500 animate-blink-wide bg-white"
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+					/>
+
+					<Dropdown isDisabled={!dropdownEnabled}>
+						<DropdownTrigger>
+							<Button className="rounded-md px-4 py-2 text-white bg-gray-800 hover:bg-gray-700">
+								Sort By: {sortBy}
+							</Button>
+						</DropdownTrigger>
+						<DropdownMenu
+							aria-label="Sort Options"
+							variant="flat"
+							selectionMode="single"
+							className="text-black bg-white shadow-lg rounded-md"
+							onAction={(e) => handleSwitchTerm(e)}
+						>
+							<DropdownItem key="Date">Date</DropdownItem>
+							<DropdownItem key="Title">Title</DropdownItem>
+						</DropdownMenu>
+					</Dropdown>
+				</div>
+
+				<div className="overflow-x-auto">
+					<table className="w-full max-w-3xl border-collapse mx-auto bg-white rounded-lg shadow-lg">
+						<thead>
+							<tr className="bg-gray-200 text-center">
+								<th
+									className="py-2 px-4 text-black rounded-tl-lg"
+									style={{ width: "70%" }}
+								>
+									Title
+								</th>
+								<th className="py-2 px-4 text-black" style={{ width: "20%" }}>
+									Date
+								</th>
+								<th
+									className="py-2 px-4 text-black rounded-tr-lg"
+									style={{ width: "10%" }}
+								></th>
+							</tr>
+						</thead>
+						<tbody>
+							{items.map((doc) => (
+								<tr
+									key={doc.id}
+									className="border-b hover:bg-gray-100 transition-colors duration-300 relative group"
+								>
+									<td className="pb-2 pt-2 pr-4" style={{ width: "70%" }}>
+										<Link
+											href={
+												"/dashboard/archivedTranscripts/transcriptcontent?id=" +
+												doc.id
+											}
+											className="text-blue-500 hover:text-blue-700 transition-colors duration-300"
+										>
+											{doc.data().title}
+										</Link>
+									</td>
+									<td
+										className="text-black pb-2 pt-2 pl-4 pr-4"
+										style={{ width: "20%" }}
+									>
+										{new Date(doc.data().time).toLocaleString()}
+									</td>
+									<td
+										className="pb-2 pt-2 pl-4 pr-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+										style={{ width: "10%" }}
+									>
+										<button
+											className="text-red-500 hover:text-red-700 transition-colors duration-300"
+											onClick={() => handleDeleteRow(doc.id)}
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												className="h-6 w-6"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke="currentColor"
+											>
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													strokeWidth={2}
+													d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+												/>
+											</svg>
+										</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/app/dashboard/archivedTranscripts/transcriptcontent/page.js
+++ src/app/dashboard/archivedTranscripts/transcriptcontent/page.js
@@ -1,8 +1,8 @@
 "use client";
 
 import { db, auth } from "@/config/firebase/config";
-import { Input } from "@nextui-org/react";
-import Link from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
+import { Link } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { onAuthStateChanged } from "firebase/auth";
@@ -43,18 +43,23 @@ export default function Home() {
     }
 
     return (
-        <div className="flex mt-20 justify-center h-screen w-screen">
-            {
-                dataDoc ? (
-                    <div>
-                        <h1 className="text-center max-w-3xl text-2xl font-bold">{dataDoc.title}</h1>
-                        <h2 className="text-center max-w-3xl text-lg font-bold">{new Date(dataDoc.time).toDateString()}</h2>
-                        <p className="max-w-3xl" >{dataDoc.data}</p>
-                    </div>
-                ) : (
-                    <h1>Loading...</h1>
-                )
-            }
+        <div className="flex flex-col items-center justify-center min-h-screen w-full">
+            {dataDoc != null ? (
+                <div className="max-w-3xl p-8 bg-white rounded-lg shadow-lg">
+                    <h1 className="text-3xl text-cyan-800 font-bold text-center mb-4">{dataDoc.title}</h1>
+                    <h2 className="text-xl text-cyan-800 font-semibold text-center mb-6">
+                        {new Date(dataDoc.time).toDateString()}
+                    </h2>
+                    <p className="text-lg text-cyan-800 leading-relaxed mb-8">{dataDoc.data}</p>
+                    <Link href="/dashboard/archivedTranscripts" className="fixed bottom-4 left-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Back
+                    </Link>
+                </div>
+            ) : (
+                <div className="flex items-center justify-center h-screen">
+                    <h1 className="text-2xl font-bold">Loading...</h1>
+                </div>
+            )}
         </div>
     );
 }
